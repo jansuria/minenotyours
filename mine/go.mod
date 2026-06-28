72bed403e98bce47866095d9b93bbd811b5ae84b
Jan <jansuria1@gmail.com>
2026-06-28 17:49:02 -0500

    Implemented a password prompt that hides the password while being typed. Includes confirming the password for encryption

--- mine/go.mod
+++ mine/go.mod
@@ -7,6 +7,7 @@ require minenotyours/fileio v0.0.0-00010101000000-000000000000
 require (
 	golang.org/x/crypto v0.53.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/term v0.44.0 // indirect
 	minenotyours/mycrypto v0.0.0-00010101000000-000000000000 // indirect
 )
 
--- mine/mine.go
+++ mine/mine.go
@@ -5,7 +5,43 @@ import (
 	"fmt"
 	"minenotyours/fileio"
 	"os"
+
+	"golang.org/x/term"
 )
+
+func ReadNewPassword() (string, error) {
+	fmt.Print("Enter Password: ")
+	password, err := term.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Print("Confirm Password: ")
+	confirmPassword, err := term.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	if err != nil {
+		return "", err
+	}
+
+	if string(password) != string(confirmPassword) {
+		return "", fmt.Errorf("passwords do not match")
+	}
+
+	return string(password), nil
+
+}
+
+func ReadPassword() (string, error) {
+	fmt.Print("Enter Password: ")
+	password, err := term.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Println()
+	if err != nil {
+		return "", err
+	}
+
+	return string(password), nil
+}
 
 func main() {
 	args := os.Args
@@ -14,39 +50,37 @@ func main() {
 		switch args[1] {
 		case "encrypt":
 			encryptCmd := flag.NewFlagSet("encrypt", flag.ExitOnError)
-			encryptPassword := encryptCmd.String("password", "", "file password")
 			encryptFile := encryptCmd.String("file", "", "path to file")
 			encryptCmd.Parse(args[2:])
-			if *encryptPassword == "" {
-				fmt.Println("Error: -password cannot be empty")
-				encryptCmd.Usage()
-				os.Exit(2)
-			}
 			if *encryptFile == "" {
 				fmt.Println("Error: -file cannot be empty")
 				encryptCmd.Usage()
 				os.Exit(2)
 			}
-			if err := fileio.CallEncryption(*encryptPassword, *encryptFile); err != nil {
+			encryptPassword, err := ReadNewPassword()
+			if err != nil {
+				fmt.Println("Error:", err)
+				os.Exit(1)
+			}
+			if err := fileio.CallEncryption(encryptPassword, *encryptFile); err != nil {
 				fmt.Println("Error: ", err)
 				os.Exit(1)
 			}
 		case "decrypt":
 			decryptCmd := flag.NewFlagSet("decrypt", flag.ExitOnError)
-			decryptPassword := decryptCmd.String("password", "", "file password")
 			decryptFile := decryptCmd.String("file", "", "path to file")
 			decryptCmd.Parse(args[2:])
-			if *decryptPassword == "" {
-				fmt.Println("Error: -password cannot be empty")
-				decryptCmd.Usage()
-				os.Exit(2)
-			}
 			if *decryptFile == "" {
 				fmt.Println("Error: -file cannot be empty")
 				decryptCmd.Usage()
 				os.Exit(2)
 			}
-			if err := fileio.CallDecryption(*decryptPassword, *decryptFile); err != nil {
+			decryptPassword, err := ReadPassword()
+			if err != nil {
+				fmt.Println("Error:", err)
+				os.Exit(1)
+			}
+			if err := fileio.CallDecryption(decryptPassword, *decryptFile); err != nil {
 				fmt.Println("Error: ", err)
 				os.Exit(1)
 			}
