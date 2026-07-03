4ee6af9299801cc1274ed1dc8651f1974b2c13fb
jansuria <jansuria1@gmail.com>
2026-07-03 12:49:14 -0400

    quick fix

--- fileio/fileio.go
+++ fileio/fileio.go
@@ -4,20 +4,18 @@ import (
 	"minenotyours/mycrypto"
 )
 
-	var parameters = &mycrypto.ArgonParameters{
-		Memory:      64 * 1024,
-		Iteration:   3,
-		Parallelism: 2,
-		SaltLength:  16,
-		KeyLength:   32,
-	}
+var parameters = mycrypto.ArgonParameters{
+	Memory:      64 * 1024,
+	Iteration:   3,
+	Parallelism: 2,
+	SaltLength:  16,
+	KeyLength:   32,
+}
 
 func CallDecryption(password string, path string) error {
-
-	return mycrypto.DecryptFile(path, password, *parameters)
+	return mycrypto.DecryptFile(path, password, parameters)
 }
 
 func CallEncryption(password string, path string) error {
-	
-	return mycrypto.EncryptFile(path, password, *parameters)
+	return mycrypto.EncryptFile(path, password, parameters)
 }
